dcd175deafb8b4c1abc48782290ffd8158f14377
nwordabuser <153253665+nwordabuser@users.noreply.github.com>
2025-08-06 11:14:33 +0530

    first question

---
+++ lab5a1.py
@@ -0,0 +1,19 @@
+#importing the necessary packages 
+import pandas as pd 
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
+#loading the dataset 
+df = pd.read_csv("/Users/niteshnirranjan/Downloads/DCT_mal.csv")
+
+#splitting the dataset into training and test set 
+X = df.iloc[:,0:1]
+Y = df.iloc[:,1:2]
+
+X_train , X_test , Y_train , Y_test = train_test_split(X,Y,test_size=0.2)
+
+#linear regression
+reg = LinearRegression().fit(X_train,Y_train)
+Y_train_pred = reg.predict(X_train)
+
+print(Y_train_pred)
